0f8b3bbe5cdc676ad8f789f2ca61caebb2205d65
George Atkinson <g.atkinson@warwick.ac.uk>
2018-10-27 14:37:16 +0100

    Added the ability to deal with disconnected graphs
    -The colorGraph method was renamed to the colorSubGraph Method
    -A new colorGraph method was created which cycles through the different
     possible subgraphs and adds the resuls together.

--- fractal-dimension.py
+++ fractal-dimension.py
@@ -1,11 +1,26 @@
 import networkx as nx
 import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
 
 
-def colorMatrix(inputGraph):
-    lMax = nx.diameter(inputGraph)
-    numberOfNodes = nx.number_of_nodes(inputGraph)
+def colorGraph(inputGraph):
+    subGraphs = list(nx.connected_component_subgraphs(inputGraph))
+    firstSubGraph = True
+    for x in subGraphs:
+        if(firstSubGraph):
+            firstSubGraphTotals = colorSubGraph(x)
+            totalBoxes = addVectors(np.zeros(len(firstSubGraphTotals)), firstSubGraphTotals)
+            firstSubGraph = False
+            print(totalBoxes)
+        else:
+            totalBoxes = addVectors(totalBoxes, colorSubGraph(x))
+    #return calculateFractalDimension(np.log(totalBoxes), len(totalBoxes))
+    return totalBoxes
+
+def colorSubGraph(inputSubGraph):
+    lMax = nx.diameter(inputSubGraph)
+    numberOfNodes = nx.number_of_nodes(inputSubGraph)
     color = np.arange(1, numberOfNodes*lMax + 1).reshape(numberOfNodes, lMax)
     coloredMatrix = np.zeros((numberOfNodes, lMax))
     for i in range(1, numberOfNodes):
@@ -15,25 +30,40 @@ def colorMatrix(inputGraph):
                 if(pathLength > lCurrent + 1):
                     coloredMatrix[i][lCurrent] = color[j][pathLength - 1]
 
-
-    lnBoxNumbers = np.zeros(lMax)
+    boxNumbers = np.zeros(lMax)
     for i in range(lMax):
-        lnBoxNumbers[i] = np.log((len(set(coloredMatrix[:, i]))))
+        boxNumbers[i] = (len(set(coloredMatrix[:, i])))
 
-    lnBoxLength = np.log(np.arange(1, 4))
-    gradient = stats.spearmanr(np.log(np.arange(1, 4)), lnBoxNumbers)[0]*(np.std(lnBoxNumbers)/np.std(lnBoxLength))
+    return boxNumbers
+
+
+def calculateFractalDimension(lnBoxNumber, maxLength):
+    lnBoxLength = np.log(np.arange(1, maxLength + 1))
+    gradient = stats.spearmanr(np.log(np.arange(1, maxLength + 1)), lnBoxNumber)[0] * (np.std(lnBoxNumber) / np.std(lnBoxLength))
     return -gradient
+
+def addVectors(vec1, vec2):
+    if(len(vec1) > len(vec2)):
+        vec2 = np.append(vec2, np.array([1]*(len(vec1) - len(vec2))))
+        return np.add(vec1, vec2)
+    if(len(vec2) > len(vec1)):
+        return addVectors(vec2, vec1)
+    else:
+        return np.add(vec1, vec2)
 
 
 if __name__ == "__main__":
     G = nx.Graph()
-    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
     G.add_edge(1, 2)
     G.add_edge(2, 3)
     G.add_edge(3, 4)
     G.add_edge(4, 5)
     G.add_edge(3, 5)
-    #plt.subplot(121)
-    #nx.draw(G, with_labels=True, font_weight='bold')
-    #plt.show()
-    print(colorMatrix(G))
+    G.add_edge(6, 7)
+    plt.subplot(121)
+    graphs = list(nx.connected_component_subgraphs(G))
+    print(len(graphs))
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+    print(colorGraph(G))
