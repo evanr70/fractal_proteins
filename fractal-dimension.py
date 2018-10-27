38fd7af329f7a4144c53ddd6c34af9a84f55f5d0
George Atkinson <g.atkinson@warwick.ac.uk>
2018-10-27 17:51:44 +0100

    Updated colorSubGraphs to be independant of node names
    -Updated the function to compare distance from a list of node names as apposed
     to assuming the nodes are numbered numerically.

--- fractal-dimension.py
+++ fractal-dimension.py
@@ -6,26 +6,21 @@ import matplotlib.pyplot as plt
 
 def colorGraph(inputGraph):
     subGraphs = list(nx.connected_component_subgraphs(inputGraph))
-    firstSubGraph = True
-    for x in subGraphs:
-        if(firstSubGraph):
-            firstSubGraphTotals = colorSubGraph(x)
-            totalBoxes = addVectors(np.zeros(len(firstSubGraphTotals)), firstSubGraphTotals)
-            firstSubGraph = False
-            print(totalBoxes)
-        else:
-            totalBoxes = addVectors(totalBoxes, colorSubGraph(x))
-    #return calculateFractalDimension(np.log(totalBoxes), len(totalBoxes))
-    return totalBoxes
+    totalBoxes = colorSubGraph(subGraphs[0])
+    for x in range(1, len(subGraphs)):
+        totalBoxes = addVectors(totalBoxes, colorSubGraph(subGraphs[x]))
+    return calculateFractalDimension(np.log(totalBoxes), len(totalBoxes))
+
 
 def colorSubGraph(inputSubGraph):
+    nodeNames = list(nx.nodes(inputSubGraph))
     lMax = nx.diameter(inputSubGraph)
     numberOfNodes = nx.number_of_nodes(inputSubGraph)
     color = np.arange(1, numberOfNodes*lMax + 1).reshape(numberOfNodes, lMax)
     coloredMatrix = np.zeros((numberOfNodes, lMax))
     for i in range(1, numberOfNodes):
         for j in range(i):
-            pathLength = len(nx.bidirectional_shortest_path(G, j + 1, i + 1)) - 1
+            pathLength = len(nx.bidirectional_shortest_path(inputSubGraph, source=nodeNames[j], target=nodeNames[i])) - 1
             for lCurrent in range(lMax):
                 if(pathLength > lCurrent + 1):
                     coloredMatrix[i][lCurrent] = color[j][pathLength - 1]
@@ -42,6 +37,7 @@ def calculateFractalDimension(lnBoxNumber, maxLength):
     gradient = stats.spearmanr(np.log(np.arange(1, maxLength + 1)), lnBoxNumber)[0] * (np.std(lnBoxNumber) / np.std(lnBoxLength))
     return -gradient
 
+
 def addVectors(vec1, vec2):
     if(len(vec1) > len(vec2)):
         vec2 = np.append(vec2, np.array([1]*(len(vec1) - len(vec2))))
@@ -54,16 +50,29 @@ def addVectors(vec1, vec2):
 
 if __name__ == "__main__":
     G = nx.Graph()
-    G.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
-    G.add_edge(1, 2)
-    G.add_edge(2, 3)
-    G.add_edge(3, 4)
-    G.add_edge(4, 5)
-    G.add_edge(3, 5)
-    G.add_edge(6, 7)
+    G.add_nodes_from(["a", "b", "c", "d", "e", "f", "g", "h", "i"])
+    G.add_edge("a", "b")
+    G.add_edge("b", "c")
+    G.add_edge("c", "d")
+    G.add_edge("d", "e")
+    G.add_edge("c", "e")
+    G.add_edge("f", "g")
+    G.add_edge("h", "i")
     plt.subplot(121)
-    graphs = list(nx.connected_component_subgraphs(G))
-    print(len(graphs))
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.show()
     print(colorGraph(G))
+
+    H = nx.Graph()
+    H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    H.add_edge(1, 2)
+    H.add_edge(2, 3)
+    H.add_edge(3, 4)
+    H.add_edge(4, 5)
+    H.add_edge(3, 5)
+    H.add_edge(6, 7)
+    H.add_edge(8, 9)
+    plt.subplot(121)
+    nx.draw(H, with_labels=True, font_weight='bold')
+    plt.show()
+    print(colorGraph(H))
