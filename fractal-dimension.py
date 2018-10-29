2f515d7d9d8b293113090c5ed6f2b1ad210b695e
George Atkinson <g.atkinson@warwick.ac.uk>
2018-10-29 18:31:07 +0000

    Implemented Compact Box Burning Algorithm
    -Created a method to check of a node is within the required radius of a set of
     nodes
    -Created a nethod to create a new candidate set from the previous candidate
     set
    -Created a method to count the number of boxes of a given length using the
     methods of the Compact Box Buring Algorithm
    -Created a method to iterate over the possible lengths of boxes

--- fractal-dimension.py
+++ fractal-dimension.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 from scipy import stats
 import matplotlib.pyplot as plt
-from random import shuffle
+import random
 import itertools
 
 
@@ -21,10 +21,10 @@ def colorSubGraph(inputSubGraph):
     numberOfNodes = nx.number_of_nodes(inputSubGraph)
     color = np.arange(1, numberOfNodes*lMax + 1).reshape(numberOfNodes, lMax)
     coloredMatrix = np.zeros((numberOfNodes, lMax))
-    if(numberOfNodes < 7):
+    if(numberOfNodes < 8):
         iterations = math.factorial(numberOfNodes)
     else:
-        iterations = 1000
+        iterations = 10000
     for k in range(iterations):
         for i in range(1, numberOfNodes):
             for j in range(i):
@@ -42,11 +42,74 @@ def colorSubGraph(inputSubGraph):
         else:
             boxNumbers = np.fromiter(map(min, boxNumbers.tolist(), currentBoxNumbers.tolist()), int)
 
-        if(numberOfNodes < 7):
+        if(numberOfNodes < 8):
             itertools.permutations(nodeNames)
         else:
-            shuffle(nodeNames)
+            random.shuffle(nodeNames)
     return boxNumbers
+
+
+def compactBoxBurnig(inputGraph):
+    subGraphs = list(nx.connected_component_subgraphs(inputGraph))
+    maxLength = nx.diameter(subGraphs[0])
+    numberOfNodes = nx.number_of_nodes(subGraphs[0])
+    totalBoxes = np.array([numberOfNodes])
+    for i in range(2, maxLength + 2):
+        totalBoxes = np.append(totalBoxes, countBoxesOfLength(subGraphs[0], i, maxLength))
+    for x in range(1, len(subGraphs)):
+        maxLength = nx.diameter(subGraphs[x])
+        numberOfNodes = nx.number_of_nodes(subGraphs[x])
+        currentBoxes = np.array([numberOfNodes])
+        for i in range(2, maxLength + 2):
+            currentBoxes = np.append(currentBoxes, countBoxesOfLength(subGraphs[0], i, maxLength))
+        totalBoxes = addVectors(totalBoxes, currentBoxes)
+    return calculateFractalDimension(np.log(totalBoxes), len(totalBoxes))
+
+
+def countBoxesOfLength(inputSubGraph, length, maxLength):
+    if(length == maxLength + 1):
+        return 1
+    else:
+        numberOfBoxes = 0
+        remainingNodes = np.array(list(nx.nodes(inputSubGraph)))
+        while(len(remainingNodes) > 0):
+            startNode = random.choice(remainingNodes)
+            evaluationSet = np.array([startNode])
+            candidateSet = np.delete(remainingNodes, np.where(remainingNodes == startNode))
+            while(len(candidateSet) > 0):
+                candidateSet = evaluatedCandidateSet(evaluationSet, candidateSet, inputSubGraph, length)
+                if(len(candidateSet) == 1):
+                    evaluationSet = np.append(evaluationSet, candidateSet[0])
+                    candidateSet = []
+                if(len(candidateSet) > 1):
+                    newTarget = random.choice(candidateSet)
+                    evaluationSet = np.append(evaluationSet, newTarget)
+                    candidateSet = np.delete(candidateSet, np.where(candidateSet == newTarget))
+            numberOfBoxes += 1
+            remainingNodes = removeElementsFromArray(remainingNodes, evaluationSet)
+        return numberOfBoxes
+
+
+def evaluatedCandidateSet(evaluationNodes, candiateSet, subGraph, length):
+    newCandidateSet = []
+    for x in candiateSet:
+        if(inUnion(evaluationNodes, x, subGraph, length)):
+            newCandidateSet.append(x)
+    return np.array(newCandidateSet)
+
+
+def inUnion(evaluationNodes, targetNode, subGraph, length):
+    for currentNode in evaluationNodes:
+        if(len(nx.bidirectional_shortest_path(subGraph, currentNode, targetNode)) > length):
+            return False
+    return True
+
+
+def removeElementsFromArray(firstArray, secondArray):
+    for x in secondArray:
+        if x in firstArray:
+            firstArray = np.delete(firstArray, np.where(firstArray == x))
+    return firstArray
 
 
 def calculateFractalDimension(lnBoxNumber, maxLength):
@@ -66,31 +129,33 @@ def addVectors(vec1, vec2):
 
 
 if __name__ == "__main__":
-    G = nx.Graph()
-    G.add_nodes_from(["a", "b", "c", "d", "e"])
-    G.add_edge("a", "b")
-    G.add_edge("b", "c")
-    G.add_edge("c", "d")
-    G.add_edge("d", "e")
-    G.add_edge("c", "e")
-    plt.subplot(121)
-    nx.draw(G, with_labels=True, font_weight='bold')
-    plt.show()
-    print(colorGraph(G))
-
-    #H = nx.Graph()
-    #H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    #H.add_edge(1, 2)
-    #H.add_edge(2, 3)
-    #H.add_edge(3, 4)
-    #H.add_edge(4, 5)
-    #H.add_edge(3, 5)
-    #H.add_edge(6, 7)
-    #H.add_edge(8, 9)
+    #G = nx.Graph()
+    #G.add_nodes_from(["a", "b", "c", "d", "e"])
+    #G.add_edge("a", "b")
+    #G.add_edge("b", "c")
+    #G.add_edge("c", "d")
+    #G.add_edge("d", "e")
+    #G.add_edge("c", "e")
     #plt.subplot(121)
-    #nx.draw(H, with_labels=True, font_weight='bold')
+    #nx.draw(G, with_labels=True, font_weight='bold')
     #plt.show()
-    #print(colorGraph(H))
+    #print(colorGraph(G))
+    #print(countBoxesOfLength(G, 2, nx.diameter(G)))
+    #print(compactBoxBurnig(G))
+
+    H = nx.Graph()
+    H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    H.add_edge(1, 2)
+    H.add_edge(2, 3)
+    H.add_edge(3, 4)
+    H.add_edge(4, 5)
+    H.add_edge(3, 5)
+    H.add_edge(6, 7)
+    H.add_edge(8, 9)
+    plt.subplot(121)
+    nx.draw(H, with_labels=True, font_weight='bold')
+    plt.show()
+    print(compactBoxBurnig(H))
 
     #J = nx.Graph()
     #J.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
