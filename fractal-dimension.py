12970a4502d608283351c9ed2823326480cafeba
George Atkinson <g.atkinson@warwick.ac.uk>
2018-10-29 14:54:12 +0000

    Added Method to permute the nodes of a network
    -Implemented functionality to permute the nodes of the box counting algorithm
     to increase the likelyhood the minmum number of boxes was calculated.

--- fractal-dimension.py
+++ fractal-dimension.py
@@ -1,7 +1,10 @@
 import networkx as nx
 import numpy as np
+import math
 from scipy import stats
 import matplotlib.pyplot as plt
+from random import shuffle
+import itertools
 
 
 def colorGraph(inputGraph):
@@ -14,21 +17,35 @@ def colorGraph(inputGraph):
 
 def colorSubGraph(inputSubGraph):
     nodeNames = list(nx.nodes(inputSubGraph))
-    lMax = nx.diameter(inputSubGraph)
+    lMax = nx.diameter(inputSubGraph) + 1
     numberOfNodes = nx.number_of_nodes(inputSubGraph)
     color = np.arange(1, numberOfNodes*lMax + 1).reshape(numberOfNodes, lMax)
     coloredMatrix = np.zeros((numberOfNodes, lMax))
-    for i in range(1, numberOfNodes):
-        for j in range(i):
-            pathLength = len(nx.bidirectional_shortest_path(inputSubGraph, source=nodeNames[j], target=nodeNames[i])) - 1
-            for lCurrent in range(lMax):
-                if(pathLength > lCurrent + 1):
-                    coloredMatrix[i][lCurrent] = color[j][pathLength - 1]
+    if(numberOfNodes < 7):
+        iterations = math.factorial(numberOfNodes)
+    else:
+        iterations = 1000
+    for k in range(iterations):
+        for i in range(1, numberOfNodes):
+            for j in range(i):
+                pathLength = len(nx.bidirectional_shortest_path(inputSubGraph, source=nodeNames[j], target=nodeNames[i])) - 1
+                for lCurrent in range(lMax):
+                    if(pathLength > lCurrent):
+                        coloredMatrix[i][lCurrent] = color[j][pathLength - 1]
 
-    boxNumbers = np.zeros(lMax)
-    for i in range(lMax):
-        boxNumbers[i] = (len(set(coloredMatrix[:, i])))
+        currentBoxNumbers = np.zeros(lMax)
+        for i in range(lMax):
+            currentBoxNumbers[i] = (len(set(coloredMatrix[:, i])))
 
+        if(k < 1):
+            boxNumbers = currentBoxNumbers
+        else:
+            boxNumbers = np.fromiter(map(min, boxNumbers.tolist(), currentBoxNumbers.tolist()), int)
+
+        if(numberOfNodes < 7):
+            itertools.permutations(nodeNames)
+        else:
+            shuffle(nodeNames)
     return boxNumbers
 
 
@@ -50,29 +67,46 @@ def addVectors(vec1, vec2):
 
 if __name__ == "__main__":
     G = nx.Graph()
-    G.add_nodes_from(["a", "b", "c", "d", "e", "f", "g", "h", "i"])
+    G.add_nodes_from(["a", "b", "c", "d", "e"])
     G.add_edge("a", "b")
     G.add_edge("b", "c")
     G.add_edge("c", "d")
     G.add_edge("d", "e")
     G.add_edge("c", "e")
-    G.add_edge("f", "g")
-    G.add_edge("h", "i")
     plt.subplot(121)
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.show()
     print(colorGraph(G))
 
-    H = nx.Graph()
-    H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    H.add_edge(1, 2)
-    H.add_edge(2, 3)
-    H.add_edge(3, 4)
-    H.add_edge(4, 5)
-    H.add_edge(3, 5)
-    H.add_edge(6, 7)
-    H.add_edge(8, 9)
-    plt.subplot(121)
-    nx.draw(H, with_labels=True, font_weight='bold')
-    plt.show()
-    print(colorGraph(H))
+    #H = nx.Graph()
+    #H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    #H.add_edge(1, 2)
+    #H.add_edge(2, 3)
+    #H.add_edge(3, 4)
+    #H.add_edge(4, 5)
+    #H.add_edge(3, 5)
+    #H.add_edge(6, 7)
+    #H.add_edge(8, 9)
+    #plt.subplot(121)
+    #nx.draw(H, with_labels=True, font_weight='bold')
+    #plt.show()
+    #print(colorGraph(H))
+
+    #J = nx.Graph()
+    #J.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    #J.add_edge(1, 2)
+    #J.add_edge(2, 3)
+    #J.add_edge(3, 4)
+    #J.add_edge(4, 5)
+    #J.add_edge(5, 6)
+    #J.add_edge(6, 7)
+    #J.add_edge(7, 8)
+    #J.add_edge(8, 9)
+    #J.add_edge(1, 6)
+    #J.add_edge(2, 5)
+    #J.add_edge(4, 9)
+    #J.add_edge(5, 8)
+    #plt.subplot(121)
+    #nx.draw(J, with_labels=True, font_weight='bold')
+    #plt.show()
+    #print(colorSubGraph(J))
