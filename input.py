f957e8e2bf0179d2f904d1becaf5243dd616279b
evanr70 <evanr70@gmail.com>
2018-10-29 12:16:03 +0000

    adding input data

--- input.py
+++ input.py
@@ -3,18 +3,26 @@ import networkx as nx
 import glob
 import matplotlib.pyplot as plt
 import os
+import time
+
+plot = False
 
 file_list = glob.glob('data/*.txt')
 
+with open('analysis/time_scale.dat', 'w+') as f:
+    print('#filename\tN\tM\tT', file=f)
+
 for file_name in file_list:
+
+    start_time = time.time()
 
     file_size = os.path.getsize(file_name)
 
-    if file_size > 5000:
-        print("skipping: {}\nsize = {}".format(file_name, file_size))
-        continue
+    # if file_size > 5000:
+    #     print("skipping: {}\nsize = {}".format(file_name, file_size))
+    #     continue
 
-    root_name = file_name[6:-3]
+    root_name = file_name[5:-3]
 
     data = np.loadtxt(file_name,
                       dtype=str,
@@ -36,14 +44,24 @@ for file_name in file_list:
 
     G.remove_edges_from(G.selfloop_edges())
 
-    plt.subplot(111)
-    nx.draw(G, with_labels=True, font_weight='bold')
+    end_time = time.time()
 
-    plt.show(block=False)
-    plt.pause(1)
-    plt.cla()
+    # if plot:
+    #     plt.subplot(111)
+    #     nx.draw(G, with_labels=True, font_weight='bold')
+    #
+    #     plt.show(block=False)
+    #     plt.pause(1)
+    #     plt.cla()
+    #
+    # print(G.edges, file=open('outputs' + '\\' + root_name + 'edgeList', 'w+'))
+    # print(G.nodes, file=open('outputs' + '\\' + root_name + 'proteinSymbols', 'w+'))
 
-    print(G.edges, file=open('outputs' + '\\' + root_name + 'edgeList', 'w+'))
-    print(G.nodes, file=open('outputs' + '\\' + root_name + 'proteinSymbols', 'w+'))
+    num_nodes = str(G.number_of_nodes())
+    num_edges = str(G.number_of_edges())
+    total_time = str(end_time - start_time)
+
+    with open('analysis/time_scale.dat', 'a') as f:
+        print("\t".join([root_name, num_nodes, num_edges, total_time]), file=f)
 
     G.clear()
---
+++ plot_one.py
@@ -0,0 +1,35 @@
+import numpy as np
+import networkx as nx
+import glob
+import matplotlib.pyplot as plt
+import os
+import time
+
+file_list = ['data/BIOGRID-ORGANISM-Human_Immunodeficiency_Virus_1-3.5.165.tab2.txt']
+
+for file_name in file_list:
+
+    root_name = file_name[5:-3]
+
+    data = np.loadtxt(file_name,
+                      dtype=str,
+                      delimiter="\t",
+                      usecols=(7, 8))
+
+    G = nx.Graph()
+
+    if not isinstance(data[0], str):
+        G.add_edges_from(data)
+    else:
+        G.add_edge(*data)
+
+    G.remove_edges_from(G.selfloop_edges())
+
+    end_time = time.time()
+
+    plt.subplot(111)
+    nx.draw(G, with_labels=False, font_weight='bold')
+
+    plt.show()
+
+    G.clear()
