c0fe1d3417381ff678d500796be2bf5e9b67e7cc
George Atkinson <g.atkinson@warwick.ac.uk>
2018-11-01 16:37:59 +0000

    Created excluded_masses function and excluded_mass function
    -excluded_mass function returns the excluded_mass of a single node in the
     network
    -excluded_masses funciton assigns excluded_masses to all the nodes of the
     network

--- fractal-dimension.py
+++ fractal-dimension.py
@@ -61,6 +61,30 @@ def count_boxes_of_length(input_sub_graph, length):
         return number_of_boxes
 
 
+# ------------------------------------------------------------------------------------------------------------
+# Implementation of the Maximum-Excluded-Mass-Burning Algorithm
+def excluded_masses(input_sub_graph, radius):
+    nodes = list(nx.nodes(input_sub_graph))
+    max_node_mass = 0
+    max_node = 0
+    for x in nodes:
+        if(input_sub_graph.nodes[x]["center"] is not True):
+            input_sub_graph.nodes[x]["excluded_mass"] = excluded_mass(input_sub_graph, radius, x)
+            if(input_sub_graph.nodes[x]["excluded_mass"] > max_node_mass):
+                max_node = x
+                max_node_mass = input_sub_graph.nodes[x]["excluded_mass"]
+    return (input_sub_graph, max_node)
+
+
+def excluded_mass(input_sub_graph, radius, node):
+    paths = nx.single_source_shortest_path(input_sub_graph, node, cutoff=radius)
+    excluded_mass = 0
+    for path in paths:
+        if(input_sub_graph.node[path]["covered"] is False and
+                input_sub_graph.node[path]["center"] is False):
+            excluded_mass += 1
+    return excluded_mass
+
 def evaluated_candidate_set(evaluation_nodes, candidate_set, sub_graph, length):
     return [x for x in candidate_set if in_union(evaluation_nodes, x, sub_graph, length)]
 
@@ -117,3 +141,98 @@ if __name__ == "__main__":
     print(compact_box_burning(H))
     node_number = 16
     iteration_number = 20
+    G = nx.Graph()
+    G.add_nodes_from(["a", "b", "c", "d", "e"])
+    G.add_edge("a", "b")
+    G.add_edge("b", "c")
+    G.add_edge("c", "d")
+    G.add_edge("d", "e")
+    G.add_edge("c", "e")
+    # plt.subplot(121)
+    # nx.draw(G, with_labels=True, font_weight='bold')
+    # plt.show()
+    # print(colourGraph(G))
+    # print(countBoxesOfLength(G, 2, nx.diameter(G)))
+    # print(compact_box_burning(G))
+    nx.set_node_attributes(G, False, "covered")
+    nx.set_node_attributes(G, False, "center")
+    (G, max_node) = excluded_masses(G, 1)
+    print(G.node["a"]["excluded_mass"])
+    print(G.node["b"]["excluded_mass"])
+    print(G.node["c"]["excluded_mass"])
+    print(G.node["d"]["excluded_mass"])
+    print(G.node["e"]["excluded_mass"])
+    print(max_node)
+
+    G.node[max_node]["center"] = True
+    G.node[max_node]["excluded_mass"] = 0
+    G.node["d"]["covered"] = True
+    G.node["e"]["covered"] = True
+    G.node["b"]["covered"] = True
+
+    (G, max_node) = excluded_masses(G, 1)
+    print(G.node["a"]["excluded_mass"])
+    print(G.node["b"]["excluded_mass"])
+    print(G.node["c"]["excluded_mass"])
+    print(G.node["d"]["excluded_mass"])
+    print(G.node["e"]["excluded_mass"])
+    print(max_node)
+
+
+    # I = nx.grid_2d_graph(3, 3)
+    # I = excluded_mass(I, 2)
+    # for x in nx.nodes(I):
+    #     print(x)
+    #     print(I.node[x]["excluded_mass"])
+    # plt.subplot(121)
+    # nx.draw(I, with_labels=True, font_weight='bold')
+    # plt.show()
+
+    # H = nx.Graph()
+    # H.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # H.add_edge(1, 2)
+    # H.add_edge(2, 3)
+    # H.add_edge(3, 4)
+    # H.add_edge(4, 5)
+    # H.add_edge(3, 5)
+    # H.add_edge(6, 7)
+    # H.add_edge(8, 9)
+    # plt.subplot(121)
+    # nx.draw(H, with_labels=True, font_weight='bold')
+    # plt.show()
+    # print(compact_box_burning(H))
+
+    # node_number = 16
+    # iteration_number = 20
+    # greedyAlTimes = testAlgorithm(colourGraph, iterNumber, nodeNumber)
+    # burningAlTimes = testAlgorithm(compactBoxBurnig, iterNumber, nodeNumber)
+    # plt.plot(np.arange(2, nodeNumber + 1), greedyAlTimes)
+    # plt.xlabel("Number Of Nodes")
+    # plt.ylabel("Time Taken (s)")
+    # plt.title("Measurements for Greedy Colouring Algorithm")
+    # plt.show()
+    #
+    # plt.plot(np.arange(2, nodeNumber + 1), burningAlTimes)
+    # plt.xlabel("Number Of Nodes")
+    # plt.ylabel("Time Taken (s)")
+    # plt.title("Measurements for Compact Box Burning Algorithm")
+    # plt.show()
+
+    # J = nx.Graph()
+    # J.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # J.add_edge(1, 2)
+    # J.add_edge(2, 3)
+    # J.add_edge(3, 4)
+    # J.add_edge(4, 5)
+    # J.add_edge(5, 6)
+    # J.add_edge(6, 7)
+    # J.add_edge(7, 8)
+    # J.add_edge(8, 9)
+    # J.add_edge(1, 6)
+    # J.add_edge(2, 5)
+    # J.add_edge(4, 9)
+    # J.add_edge(5, 8)
+    # plt.subplot(121)
+    # nx.draw(J, with_labels=True, font_weight='bold')
+    # plt.show()
+    # print(colourSubGraph(J))
