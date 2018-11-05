8a04dc1e82a60490b754da8c1f8ba4c4aa0665e9
George Atkinson <george.atkinson@dtc.ox.ac.uk>
2018-11-05 09:01:54 +0000

    Corrected problems with test_fractal_dimension.py

--- algorithm_tests/test_fractal_dimension.py
+++ algorithm_tests/test_fractal_dimension.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     mean = 0.01*node_number*(node_number - 1)
     graph = nx.fast_gnp_random_graph(node_number, (2*mean)/(node_number*(node_number - 1)))
     mass_boxes = fd.maximum_excluded_mass_burning(graph)
-    print(mass_boxes)
     # plt.subplot(121)
     # nx.draw(graph, with_labels=True, font_weight='bold')
     # plt.show()
@@ -23,11 +22,11 @@ if __name__ == "__main__":
     graph_fd = fd.calculate_fractal_dimension(np.log(mass_boxes), len(mass_boxes))
     boxes_spearman_theory = []
     boxes_pearson_theory = []
-    print(graph_fd_spearman)
-    print(graph_fd)
+
     for length in lengths:
         boxes_spearman_theory.append(box_theory(length, graph_fd_spearman, node_number))
         boxes_pearson_theory.append(box_theory(length, graph_fd, node_number))
+    plt.figure()
     plt.plot(lengths, mass_boxes, label="Actual")
     plt.plot(lengths, boxes_spearman_theory, label="Theoretical based on Spearman")
     plt.plot(lengths, boxes_pearson_theory, label="Theoretical based on Pearson")
@@ -36,6 +35,7 @@ if __name__ == "__main__":
     plt.legend()
     plt.savefig("../graphs/no_of_boxes.png")
 
+    plt.figure()
     plt.plot(np.log(lengths), np.log(mass_boxes), label="Actual")
     plt.plot(np.log(lengths), np.log(boxes_spearman_theory), label="Theoretical based on Spearman")
     plt.plot(np.log(lengths), np.log(boxes_pearson_theory), label="Theoretical based on Pearson")
@@ -43,3 +43,7 @@ if __name__ == "__main__":
     plt.ylabel("Log Number of Boxes")
     plt.legend()
     plt.savefig("../graphs/log_no_of_boxes.png")
+    print("Difference between Spearman's approximation and actual number of boxes.")
+    print(np.subtract(mass_boxes, boxes_spearman_theory))
+    print("Difference between Pearson's approximation and actual number of boxes.")
+    print(np.subtract(mass_boxes, boxes_pearson_theory))
