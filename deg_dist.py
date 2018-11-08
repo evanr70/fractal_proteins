9db71f37244e875a695d7b41f2bfd2627fa2070c
evanr70 <evanr70@gmail.com>
2018-11-08 14:09:03 +0000

    degree distribution jupyter notebook and jlog analysis

--- deg_dist.py
+++ deg_dist.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import os
 import operator
 import time
+import graph_magic
 
 # file_name = 'data/BIOGRID-ORGANISM-Saccharomyces_cerevisiae_S288c-3.5.165.tab2.txt'
 # file_name = 'data/BIOGRID-ORGANISM-Homo_sapiens-3.5.165.tab2.txt'
@@ -15,25 +16,19 @@ import time
 
 file_list = glob.glob('data/*.txt')
 
-fig = plt.figure(figsize=(1920, 949))
+fig = plt.figure()
 
 for file_name in file_list:
 
-    root_name = file_name[5:-3]
+    root_name = file_name.split('-')[-2]
 
-    data = np.loadtxt(file_name,
-                      dtype=str,
-                      delimiter="\t",
-                      usecols=(7, 8))
+    print(root_name)
 
-    G = nx.Graph()
+    G = graph_magic.get_graph_from_file(file_name, largest_only=False)
 
-    if not isinstance(data[0], str):
-        G.add_edges_from(data)
-    else:
-        G.add_edge(*data)
-
-    G.remove_edges_from(G.selfloop_edges())
+    if len(G) < 1000:
+        print("only {} nodes".format(len(G)))
+        continue
 
     # plt.subplot(111)
     # nx.draw(G, with_labels=True, font_weight='bold')
@@ -51,19 +46,19 @@ for file_name in file_list:
 
     deg, cnt = zip(*degree_count.items())
 
-    print(degree_count)
+    # print(degree_count)
 
     plt.bar(deg, cnt, width=0.80, color='b')
 
-    plt.title("Degree histogram")
+    plt.title("{} - {} nodes".format(root_name.replace('_', ' '), len(G)))
     plt.ylabel("Count")
     plt.xlabel("Degree")
-    plt.xlim(0, 550)
+    plt.xlim(0, min(550, max(degree_sequence)))
 
     plt.show(block=False)
     plt.pause(0.1)
 
-    plt.savefig('analysis/deg_dist/' + root_name + 'png')
+    plt.savefig('analysis/deg_dist/' + file_name.split('-')[-2] + '-deg_dist.png')
 
     G.clear()
     plt.cla()
